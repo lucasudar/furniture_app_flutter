576c760242335312fdefe9d2a8835363817b2add
Nikita Starkov <nikita.starkov@tietoevry.com>
2022-04-17 13:38:53 -0700

    fixed null for sizeConfig

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:furniture_app_flutter/size_config.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    SizeConfig().init(context);
     return Scaffold(
-      appBar: AppBar()
-    );
+        appBar: AppBar(
+      leading: IconButton(
+        icon: SvgPicture.asset('assets/icons/menu.svg',
+            height: SizeConfig.defaultSize! * 2),
+        onPressed: () {},
+      ),
+    ));
   }
 }
